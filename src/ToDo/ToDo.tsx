343c0dc12233791f4bf57c3a03caa70b93fc0dd7
nishanth <mksabarinishanth@gmail.com>
2025-08-23 16:05:00 +0530

    Delete from server added

--- src/ToDo/ToDo.tsx
+++ src/ToDo/ToDo.tsx
@@ -5,6 +5,27 @@ function ToDo() {
   const [task, setTask] = useState('');
   const [todoData, setTodoData] = useState([]);
 
+  async function handleFetch() {
+    const fetchedDataFromTheServer = await fetchFromServer();
+    console.log(fetchedDataFromTheServer);
+    setTodoData(fetchedDataFromTheServer);
+  }
+
+  async function handlePost() {
+    const postedData = await postToTheServer(task, false);
+    handleFetch();
+  }
+
+  async function handlePatchCompleteStatus(id, completed) {
+    const editedData = await editFromTheServer(id, completed);
+    handleFetch();
+  }
+
+  async function handleDelete(id) {
+    const delData = await deleteFromTheServer(id, deleteFromTheServer);
+    handleFetch();
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-header">
@@ -15,7 +36,7 @@ function ToDo() {
           <form
             onSubmit={e => {
               e.preventDefault();
-              upDateToTheServer(task, 'false', setTodoData);
+              handlePost();
               setTask('');
             }}
           >
@@ -33,7 +54,12 @@ function ToDo() {
         </div>
         <div className="todo-display">
           {todoData.map(e => (
-            <DisplayToDo key={e.id} task={e.task}></DisplayToDo>
+            <DisplayToDo
+              key={e.id}
+              task={e}
+              handlePatchCompleteStatus={handlePatchCompleteStatus}
+              handleDelete={handleDelete}
+            ></DisplayToDo>
           ))}
         </div>
       </div>
@@ -43,7 +69,7 @@ function ToDo() {
 
 export default ToDo;
 
-async function upDateToTheServer(task, completed, setTodoData) {
+async function postToTheServer(task, completed) {
   const url = 'http://127.0.0.1:8090/api/collections/todo/records';
   try {
     const res = await fetch(url, {
@@ -58,9 +84,6 @@ async function upDateToTheServer(task, completed, setTodoData) {
     });
     const data = await res.json();
     console.log(data);
-    const latestData = await fetchFromServer();
-    console.log(latestData);
-    setTodoData(latestData);
   } catch (error) {
     console.log(error);
   }
@@ -79,13 +102,49 @@ async function fetchFromServer() {
   }
 }
 
-function DisplayToDo({ task }) {
+async function editFromTheServer(id, completed) {
+  const url = `http://127.0.0.1:8090/api/collections/todo/records/${id}`;
+  try {
+    const res = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        completed: !completed,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteFromTheServer(id) {
+  const url = `http://127.0.0.1:8090/api/collections/todo/records/${id}`;
+  try {
+    const res = await fetch(url, { method: 'DELETE' });
+  } catch (error) {}
+}
+// Display to do:
+
+function DisplayToDo({ task, handlePatchCompleteStatus, handleDelete }) {
+  console.log(task);
+  const cN = `todoactual ${task.completed ? 'completedlt' : 'none'}`;
   return (
     <div className="todo">
-      <div className="todoactual">{task}</div>
+      <div className={cN}>{task.task}</div>
       <button className="edit">Edit</button>
-      <button className="delete">Delete</button>
-      <button className="completed">Completed</button>
+      <button className="delete" onClick={() => handleDelete(task.id)}>
+        Delete
+      </button>
+      <button
+        className={task.completed ? 'completedlt' : 'completed'}
+        onClick={() => handlePatchCompleteStatus(task.id, task.completed)}
+      >
+        {task.completed ? 'Undo' : 'Completed'}
+      </button>
     </div>
   );
 }
